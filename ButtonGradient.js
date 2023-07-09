ba1547c8f415ef4b8ef90ac5ca9d21090a5e49e4
Kevin Latino <latinokevin9@gmail.com>
2023-07-09 12:54:30 -0600

    hola

---
+++ ButtonGradient.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { StyleSheet, TextInput, Text, View, TouchableOpacity } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default function ButtonGradient(){
+    return(
+        <TouchableOpacity style={styles.container} >
+         <LinearGradient
+        colors={['#e00000', '#ff4040', "#ff7676"]}
+        start={{x: 1, y: 0}}
+        end={{x:0,  y: 1}}
+        style={styles.button}
+        >
+        <Text style={styles.text}>Iniciar sesión</Text>
+      </LinearGradient>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    text: {
+     fontSize: 14, 
+     color: "white",
+     fontWeight: "bold"
+    },
+    button:{
+  height: 50,
+  width: "80%",
+  marginTop: 20,
+  borderRadius: 25,
+  padding: 17,
+  alignItems: "center",
+  justifyContent: "center"
+    },
+
+  }); 
