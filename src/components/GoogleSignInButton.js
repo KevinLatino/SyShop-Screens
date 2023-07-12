a893fbbe0ad0594dd82c3b4d5b761a5e47641d97
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2023-07-11 19:26:09 -0600

    Login Google

--- src/components/GoogleSignInButton.js
+++ src/components/GoogleSignInButton.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { TouchableOpacity, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import * as Google from 'expo-auth-session/providers/google'
 import axios from 'axios'
 import { maybeCompleteAuthSession } from "expo-web-browser"
@@ -9,25 +9,34 @@ maybeCompleteAuthSession()
 
 const styles = {
   button: {
-    padding: 30,
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 15,
+    paddingRight: 15,
     borderRadius: 3,
+    borderWidth: 1,
     boxShadow: "0 -1px 0 rgba(0, 0, 0, .04), 0 1px 1px rgba(0, 0, 0, .25)",
+    borderColor: "silver",
     color: "#757575",
-    backgroundImage: "url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4=)",
     backgroundColor: "white",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "12px 11px"
+    width: "fit-content"
+  },
+  buttonInnerView: {
+    flexDirection: "row",
+    gap: 15,
+    justifyContent: "center",
+    alignItems: "center",
   },
   buttonText: {
-    fontSize: 14,
+    fontSize: 16,
     fontWeight: 500,
-    fontFamily: "-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif",
+    fontFamily: "-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif"
   }
 }
 
-const getUserInformation = (accessToken) => {
+const getUserInformation = async (accessToken) => {
   const googleUrl = "https://www.googleapis.com/userinfo/v2/me"
-  const { data } = axios.get(googleUrl, {
+  const { data } = await axios.get(googleUrl, {
     headers: {
       Authorization: `Bearer ${accessToken}`
     }
@@ -42,28 +51,29 @@ export default ({ setUserInformation }) => {
     androidClientId: configuration.GOOGLE_ANDROID_CLIENT_ID
     // iosClientId: ""
   })
+  const googleLogoUri = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2008px-Google_%22G%22_Logo.svg.png"
 
   useEffect(() => {
     if ((response !== null) && (response.type === "success")) {
       const accessToken = response.authentication.accessToken
-      const userInformation = getUserInformation(accessToken)
 
-      setUserInformation(userInformation)
+      getUserInformation(accessToken)
+        .then((userInformation) => setUserInformation(userInformation))
     }
   }, [response])
 
   return (
     <TouchableOpacity
       style={styles.button}
-      disabled={request !== null}
-      onPress={() => {
-        console.log("hollla")
-        promptAsync()
-      }}
+      onPress={() => promptAsync()}
     >
-      <Text style={styles.text}>
-        Sign in with Google
-      </Text>
+      <View style={styles.buttonInnerView}>
+        <Image source={{uri: googleLogoUri, width: 25, height: 25}} />
+
+        <Text style={styles.buttonText}>
+          Sign in with Google
+        </Text>
+      </View>
     </TouchableOpacity>
   )
 }
