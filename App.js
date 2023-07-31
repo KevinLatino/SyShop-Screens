2a435be59bc8db400f30a424af005556706024e8
KevinLatino <latinokevin9@gmail.com>
2023-07-31 08:19:08 -0600

    foo

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
-import { PaperProvider } from 'react-native-paper'
-import SearchBar from './src/components/SearchBar'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, TextInput, Text, View } from 'react-native';
+import React from 'react';
 
-export default () => {
+
+export default function App() {
   return (
-    <PaperProvider>
-      <SearchBar />
-    </PaperProvider>
-  )
+    <View style={styles.container}>
+            <Text style={styles.title }>Bienvenido </Text>
+      <Text style={styles.subtitle}>¡Inicia sesión para comenzar!</Text>
+      <TextInput style={styles.TextInput} placeholder="Correo eléctronico"/>
+      <TextInput style={styles.TextInput} placeholder='Contraseña '/>
+
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f1f1f1 ',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 50,
+    color: "#344340",
+    fontWeight: "bold"
+  },
+  subtitle: {
+    fontSize: 20,
+    color: "gray",
+  }, 
+  TextInput: {
+    borderWidth: 1, 
+    paddingStart: 30,
+    borderColor: "gray",
+    padding: 10,
+    width: "80%",
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: "#fff"
+  },
+  button:{
+
+  }
+});
