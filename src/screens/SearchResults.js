bee74e546804ebdcac405bfdc0e07bae029e4ce1
Your Name <jpappa2603@gmail.com>
2023-08-01 15:09:53 -0600

    Implementé el screen de los resultados de una búsqueda en el screen de inicio

--- src/screens/SearchResults.js
+++ src/screens/SearchResults.js
@@ -1,18 +1,33 @@
+import { useState } from 'react'
 import { useRoute } from '@react-navigation/native'
 import useCounter from '../hooks/useCounter'
 import { useQuery, requestServer } from '../utilities/requests'
 import ScrollView from '../components/ScrollView'
+import PostTile from '../components/PostTile'
+import Slider from '@miblanchard/react-native-slider'
 import {
     View,
     ScrollView as ReactNativeScrollView,
     StyleSheet
 } from 'react-native'
-import { Chip, Text } from 'react-native-paper'
+import {
+    Card,
+    Chip,
+    IconButton,
+    Text,
+    Menu,
+    ActivityIndicator,
+    Button
+} from 'react-native-paper'
 
 const styles = StyleSheet.create({
-    scrollView: {
+    horizontalScrollView: {
         rowGap: "0.5rem",
         padding: "0.3rem"
+    },
+    searchedTextDisplay: {
+        padding: "0.5rem",
+        borderRadius: 10
     }
 })
 
@@ -30,49 +45,241 @@ const fetchStores = async (searchedName, pageNumber) => {
     return stores
 }
 
-const CategoriesScrollView = ({ categoriesNames }) => {
+const CategoryChip = ({ category }) => {
+    return (
+        <Chip
+            icon="shape"
+            style={{ width: "fit-content" }}
+        >
+            {category}
+        </Chip>
+    )
+}
+
+const StoreTile = ({ store }) => {
+    return (
+        <Card>
+            <Card.Cover source={{ uri: store.picture }} />
+
+            <Card.Title title={store.name} />
+
+            <Card.Content>
+                <Text variant="bodyMedium">
+                    {store.description}
+                </Text>
+            </Card.Content>
+        </Card>
+    )
+}
+
+const SearchedTextDisplay = ({ searchedText }) => {
+    return (
+        <View style={styles.searchedTextDisplay}>
+            <Text variant="bodyMedium">
+                {searchedText}
+            </Text>
+        </View>
+    )
+}
+
+const SortingPropertyFilterMenu = ({
+    sortingProperty,
+    onSelect,
+    isVisible,
+    setIsVisible
+}) => {
+    const anchor = (
+        <Button
+            mode="outlined"
+            onPress={() => setIsVisible(true)}
+        >
+            {
+                sortingProperty === "publication_date"
+                    ? "Fecha de publicación"
+                    : "Precio"
+            }
+        </Button>
+    )
+
+    return (
+        <Menu
+            visible={isVisible}
+            onDismiss={() => setIsVisible(false)}
+            anchor={anchor}
+        >
+            <Menu.Item
+                title="Fecha de publicación"
+                onPress={() => onSelect("publication_date")}
+            />
+
+            <Menu.Item
+                title="Precio"
+                onPress={() => onSelect("price")}
+            />
+        </Menu>
+    )
+}
+
+const PostsResultsFilters = ({ filters, onChangeFilters }) => {
+    const {
+        minimumPrice,
+        maximumPrice,
+        sortingProperty,
+        sortingSchema
+    } = filters
+    const [isMenuVisible, setIsMenuVisible] = useState(false)
+
+    return (
+        <View>
+            <View>
+                <IconButton
+                    mode="outlined"
+                    icon={
+                        sortingSchema === "ascending"
+                        ? "arrow-up-drop-circle-outline"
+                        : "arrow-down-drop-circle-outline"
+                    }
+                    onPress={() => onChangeFilters({
+                        ...filters,
+                        sortingSchema: sortingSchema === "ascending"
+                            ? "descending"
+                            : "ascending"
+                    })}
+                />
+
+                <SortingPropertyFilterMenu
+                    isVisible={isMenuVisible}
+                    setIsVisible={setIsMenuVisible}
+                    sortingProperty={sortingProperty}
+                    onSelect={(sortingProperty) => onChangeFilters({
+                        ...filters,
+                        sortingProperty
+                    })}
+                />
+            </View>
+
+            <View>
+                <Slider
+                    minimumValue={0}
+                    maximumValue={maximumPrice}
+                    step={1000}
+                    value={[minimumPrice, maximumPrice]}
+                    onValueChange={([minimum, maximum]) => onChangeFilters({
+                        ...filters, minimumPrice: minimum, maximumPrice: maximum
+                    })}
+                />
+            </View>
+        </View>
+    )
+}
+
+const SearchedCategoriesScrollView = ({ categoriesNames }) => {
     const categoriesNamesChips = categoriesNames.map((categoryName) => {
         return (
-            <Chip
+            <CategoryChip
                 key={categoryName}
-                icon="shape"
-                style={{ width: "fit-content" }}
-            >
-                {categoryName}
-            </Chip>
+                category={categoryName}
+            />
         )
     })
 
     return (
         <ReactNativeScrollView
             horizontal
-            style={styles.scrollView}
+            style={styles.horizontalScrollView}
         >
             {categoriesNamesChips}
         </ReactNativeScrollView>
     )
 }
 
-const StoresScrollView = ({ stores, ...scrollViewProps }) => {
+const StoresResultsScrollView = ({ searchedText }) => {
+    const pageNumber = useCounter()
+    const storesQuery = useQuery(() => fetchStores(searchedText, pageNumber.value))
+
+    if (storesQuery.result === null) {
+        return (
+            <View>
+                <ActivityIndicator animating />
+            </View>
+        )
+    }
+
+    const storesCards = storesQuery.result.map((store) => {
+        return (
+            <StoreTile
+                key={store.user_id}
+                store={store}
+            />
+        )
+    })
+
     return (
         <ReactNativeScrollView
             horizontal
-            style={styles.scrollView}
+            style={styles.horizontalScrollView}
+            onEndReached={pageNumber.increment}
         >
+            {storesCards}
         </ReactNativeScrollView>
+    )
+}
+
+const PostsResults = ({ searchedText }) => {
+    const maximumPriceQuery = useQuery(() => fetchMaximumPrice())
+
+    if (maximumPriceQuery.result === null) {
+        return (
+            <View>
+                <ActivityIndicator animating />
+            </View>
+        )
+    }
+
+    const pageNumber = useCounter()
+    const [searchFilters, setSearchFilters] = useState({
+        minimumPrice: 0,
+        maximumPrice: maximumPriceQuery.result,
+        sortingProperty: "publicacion_date",
+        sortingSchema: "descending"
+    })
+    const postsQuery = useQuery(() => fetchPosts(
+        searchedText,
+        filters,
+        pageNumber.value
+    ))
+
+    const handleChangeFilters = (newSearchFilters) => {
+        setSearchFilters(newSearchFilters)
+
+        postsQuery.refresh()
+    }
+
+    return (
+        <View>
+            <PostsResultsFilters
+                filters={searchFilters}
+                onChangeFilters={handleChangeFilters}
+            />
+
+            <ScrollView
+                data={postsQuery.result}
+                renderItem={(post) => <PostTile post={post} />}
+                onEndReached={pageNumber.increment}
+            />
+        </View>
     )
 }
 
 export default () => {
     const route = useRoute()
-    const storesPageNumber = useCounter()
-
     const { text, categoriesNames } = route.params
-    const storesQuery = useQuery(() => fetchStores(text, storesPageNumber.value))
 
     return (
         <View>
-            <CategoriesScrollView
+            <SearchedTextDisplay searchedText={text} />
+
+            <SearchedCategoriesScrollView
                 categoriesNames={categoriesNames}
             />
 
@@ -80,9 +287,16 @@ export default () => {
                 Tiendas
             </Text>
 
-            <StoresScrollView
-                stores={storesQuery.result}
-                onEndReached={storesPageNumber.increment}
+            <StoresResultsScrollView
+                searchedText={text}
+            />
+
+            <Text variant="titleMedium">
+                Publicaciones
+            </Text>
+
+            <PostsResults
+                searchedText={text}
             />
         </View>
     )
