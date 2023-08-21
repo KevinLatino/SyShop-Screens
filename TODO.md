d98b9e37fc5221a6ea400ace241d939b02d6d690
Gabriel Coronel <gabrielcoronel0303@gmail.com>
2023-08-20 20:06:32 -0600

    Implementé el ingrese de una imagen al registrarse 1. Se añadió un input para imágenes (PictureInput) 2. Se autorellena el input para imágenes con los datos de Google 3. Se corregió el error lógico que no mostraba los inputs para correo electrónico y contraseña

--- TODO.md
+++ TODO.md
@@ -1,8 +1,3 @@
-# Último día
-- [ ] Arreglar problemas de lógica en SignUp y SignIn
-- [ ] Implementar ingresar una foto a la hora de crear una cuenta (con Google también)
-- [ ] Terminar la navegación entre todos los screens
-
 # Tercera defensa
 - [ ] Manejar errores del servidor
 - [ ] Hacer la interfaz crack
---
+++ src/components/PictureInput.js
@@ -0,0 +1,21 @@
+import { TouchableRipple, Avatar } from 'react-native-paper'
+import { selectPictureFromGallery } from '../utilities/camera'
+
+export default ({ picture, onChangePicture, ...avatarProps }) => {
+  const handleChangePicture = async () => {
+    const newPicture = await selectPictureFromGallery()
+
+    onChangePicture(newPicture)
+  }
+
+  return (
+    <TouchableRipple
+      onPress={handleChangePicture}
+    >
+      <Avatar.Image
+        source={{ uri: picture }}
+        {...avatarProps}
+      />
+    </TouchableRipple>
+  )
+}
